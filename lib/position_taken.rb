5829cd1beea9af47974973b191308bf11b221fff
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-01 21:44:54 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,9 +8,11 @@
 # end
 
 def position_taken?(board, index)
-  if (board[index] == "" || " ")
-    false
-  else
+  if board[index] == "X" 
     true
+  elsif board[index] == "O"
+    true
+  else 
+    false
   end
 end
